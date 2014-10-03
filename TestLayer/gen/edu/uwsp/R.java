334e894bd9fbed95afea12da7e5c3295d8cc8f1e
brobi665 <brobi665@SCIA203-WD-05.uwsp.edu>
2014-10-02 21:36:28 -0500

    Replaced location service activation by layer additions. Location now activated by a button.

--- TestLayer/gen/edu/uwsp/R.java
+++ TestLayer/gen/edu/uwsp/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_location=0x7f020001;
     }
     public static final class id {
         public static final int location=0x7f050000;
@@ -23,6 +24,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
-        public static final int text=0x7f040002;
+        public static final int location_description=0x7f040002;
     }
 }
